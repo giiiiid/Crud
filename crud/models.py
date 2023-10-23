073639239ea6c632f76392c8ee9972f508740502
Giiid <gideonfummey1@gmail.com>
2023-10-23 18:18:41 +0200

    Cleared errors

--- crud/models.py
+++ crud/models.py
@@ -4,7 +4,6 @@ import datetime
 class Crud(models.Model):
     activity = models.CharField(max_length=200, default='')
     location = models.CharField(max_length=200, default='')
-    # time = models.TimeField(blank=True, null=True, default=datetime.time)
 
     def __str__(self):
         return self.activity
