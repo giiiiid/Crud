187660fb95710ca8425f1953976199ff25059dd3
Giiid <gideonfummey1@gmail.com>
2023-11-23 07:11:28 +0100

    Tests for dynamic urls

--- crud/tests/test_url.py
+++ crud/tests/test_url.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from ..views import index, user_home, detail, update, done, delete, search, sign_up, login_user, logout_user
+from ..views import index, user_home, detail, update, done, delete, search, sign_up, login_user, logout_user, crudApi
 
 
 class TestUrls(SimpleTestCase):
@@ -13,9 +13,29 @@ class TestUrls(SimpleTestCase):
         url = reverse('home')
         self.assertEquals(resolve(url).func, user_home)
     
-    def test_detail_resolved(self):
+    def test_search_resolved(self):
         url = reverse('search')
         self.assertEquals(resolve(url).func, search)
+    
+    def test_detail_resolved(self):
+        w = self
+        url = reverse('detail', args=['activity'])
+        self.assertEquals(resolve(url).func, detail)
+    
+    def test_update_resolved(self):
+        w = self
+        url = reverse('update', args=['activity'])
+        self.assertEquals(resolve(url).func, update)
+    
+    def test_done_resolved(self):
+        w = self
+        url = reverse('done', args=['activity'])
+        self.assertEquals(resolve(url).func, done)
+    
+    def test_delete_resolved(self):
+        w = self
+        url = reverse('delete', args=['activity'])
+        self.assertEquals(resolve(url).func, delete)
     
     def test_signup_resolved(self):
         url = reverse('signup')
@@ -28,8 +48,8 @@ class TestUrls(SimpleTestCase):
     def test_logout_resolved(self):
         url = reverse('logout')
         self.assertEquals(resolve(url).func, logout_user)
-    
-    # def test_detail_resolved(self):
-    #     w = self
-    #     url = reverse('detail', kwargs={'activity':w.activity})
-    #     self.assertEquals(resolve(url).func, detail)
+
+    def test_crudApi_resolved(self):
+        url = reverse('api')
+        # print(resolve(url))
+        self.assertEquals(resolve(url).func, crudApi)
